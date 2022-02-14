20e652ab2e6d2369b8e673e69d8fb6071966e043
jbarrera3 <jbarrera@titamedia.com>
2022-02-14 12:56:15 -0500

    concurrency

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
 import http from "http";
+const throng = require('throng')
+
 const { connect } = require("./db/Config");
 import { routes } from "./router";
 
 const port = process.env.PORT || "8000";
+const WORKERS = process.env.WEB_CONCURRENCY || 1
+
+throng({
+  workers: WORKERS,
+  lifetime: Infinity
+}, createServer);
 
 export function createServer() {
   const app = http.createServer(async (req: any, res: any) => {
